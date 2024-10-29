df097a34b77690a6af8ce8254b20f395170d3f7f
FernandoNogueiraPy <fernandomoltt101@gmail.com>
2024-10-29 20:09:39 -0300

    chore: Add .gitignore, main.py, requirements.txt, and error classes

---
+++ main.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+from src.routers import router
+
+
+app = FastAPI()
+
+
+app.include_router(router)
+
+HOST = "0.0.0.0"
+PORT = 8000
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=HOST, port=PORT)
